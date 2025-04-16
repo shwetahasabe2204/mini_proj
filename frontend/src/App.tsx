b3da7d5db044ac3bb40f6ae99732da06054e0102
shwetahasabe2204 <shwetahasabe18@gmail.com>
2025-04-16 15:43:42 -0700

    ui update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { RecoilRoot } from 'recoil';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Layout from './components/Layout';
+import PropertyDetails from './components/PropertyDetails';
 
 function App() {
   return (
@@ -12,8 +15,22 @@ function App() {
         <BrowserRouter>
           <ToastContainer />
           <Routes>
-            <Route path="/" element={<Login />}></Route>
-            <Route path="/login" element={<Login />}></Route>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path='dashboard'element={
+              <Layout>
+                <Dashboard />
+              </Layout>
+            } />
+            <Route
+              path="/property/:id" 
+                  element={
+                            <Layout>
+                              <PropertyDetails />
+                            </Layout>
+                          }
+                  >
+            </Route>   
           </Routes>
         </BrowserRouter>
       </RecoilRoot>
---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,23 @@
+import Card from '@/components/Card'
+import { properties } from '../constants/Properties'
+
+const Dashboard = () => {
+  return (
+    
+      <div className='grid grid-cols-4 gap-8 mt-8'>
+
+        {properties.map((property)=>(
+          <Card
+            id={property.id}
+            image={property.image[0]}
+            text={property.title}
+            address={property.address}
+          />
+        ))}
+        
+      </div>
+    
+  )
+}
+
+export default Dashboard
